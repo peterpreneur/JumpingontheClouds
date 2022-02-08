36ef2253d9ceb55f99a6893b3154ca7d5be62beb
pas8 <peterpreneur@gmail.com>
2022-02-08 15:02:31 -0600

    final

--- JumpingontheClouds/src/JumpingontheClouds/Result.java
+++ JumpingontheClouds/src/JumpingontheClouds/Result.java
@@ -1,5 +1,37 @@
 package JumpingontheClouds;
 
-public class Result {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
+class Result {
+
+	/*
+	 * Complete the 'jumpingOnClouds' function below.
+	 *
+	 * The function is expected to return an INTEGER. The function accepts
+	 * INTEGER_ARRAY c as parameter.
+	 */
+
+	// public static int jumpingOnClouds(List<Integer> c) {
+	public static void main(String[] args) {
+		List<Integer> c = new ArrayList<>();
+		
+		//Enter number of steps/Array indexes
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		
+		//Enter steps based on number of steps above
+		for (int i=0; i < size; i++) {
+			c.add(in.nextInt());
+		}		
+		System.out.println(c.toString());
+
+        int count = -1;	
+        for (int j = 0; j < size; j++, count++) {
+            if ((j < size-2) && (c.get(j+2) == 0)) j++;
+        }
+        System.out.println(count);		
+	}
 }
